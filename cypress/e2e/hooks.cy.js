fa9fb8ab1e39613c520623e95fa451562b647106
Abhishek Singh <abhishekgps006@gmail.com>
2024-10-30 14:52:36 +0530

    added hooks example and and few more example in demo

--- cypress/e2e/hooks.cy.js
+++ cypress/e2e/hooks.cy.js
@@ -1,23 +1,23 @@
 /// <reference types="Cypress" />
 describe('My Test Scenarios', () => {
-  it('Test case 1', () => {
+  beforeEach(()=>{
     cy.visit('https://example.com')
+  })
+  it('Test case 1', () => {
     cy.url().should('contain','example.com')
     
   })
 
   it('My Second test case - assert title', () => {
-    cy.visit('https://example.com')
     cy.title().should('contain','Example Domain')
   })
+  // run only this test case
+  // it.only('My 3rd test case - assert element', () => {
+  //   cy.get('h1').should('be.visible')
+  // })
 
-  it('My 3rd test case - assert element', () => {
-    cy.visit('https://example.com')  
-    cy.get('h1').should('be.visible')
-  })
-
-  it('My 4th test case - reload and logs', () => {
-    cy.visit('https://example.com')  
+  // bug id #124
+  it.skip('My 4th test case - reload and logs', () => { 
     cy.log('before load')
     cy.reload()
     cy.log('after reload')
